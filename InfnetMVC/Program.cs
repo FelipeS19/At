d3405cbc7df252becc8069d0d5b0604052a5d017
Felipe Sodré <65314804+FelipeS19@users.noreply.github.com>
2024-04-05 00:17:55 -0300

    Update Program.cs

--- InfnetMVC/Program.cs
+++ InfnetMVC/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Funcionarios}/{action=Index}/{id?}");
 
 app.Run();
